66e0542ebf7a1de49a4f12572b28d7b06ead8c39
割と普通 <warito_hutsu@hotmail.com>
2014-03-05 23:28:45 +0900

    StringUtil commit

---
+++ WorldChallenge.Test/StringUtilTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WorldChallenge.Test
+{
+    using WorldChallenge;
+
+    [TestClass]
+    public class StringUtilTest
+    {
+        [TestMethod]
+        public void TestMethod_同じ長さの並べ替え()
+        {
+            bool expected = true;
+            bool actual = StringUtil.IsRearrange("abcde", "ebdac");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod_同じ長さで並べ替えでない()
+        {
+            bool expected = false;
+            bool actual = StringUtil.IsRearrange("abcde", "ebdfc");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod_第一引数の方が長い()
+        {
+            bool expected = false;
+            bool actual = StringUtil.IsRearrange("abcde", "ebac");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod_第二引数の方が長い()
+        {
+            bool expected = false;
+            bool actual = StringUtil.IsRearrange("abde", "ebdac");
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ WorldChallenge/StringUtil.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldChallenge
+{
+    static public class StringUtil
+    {
+        static public bool IsRearrange(string str1, string str2)
+        {
+            if (str1.Length != str2.Length) return false;
+            var ret = str1.ToArray().Except(str2.ToArray());
+            return !ret.Any();
+        }
+    }
+}
